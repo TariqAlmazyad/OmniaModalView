5ed27d5fa723ce6abc0bc949e031db0570e8d6ca
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2021-05-16 11:17:09 +0300

    init

--- Sources/OmniaModalView/OmniaModalView.swift
+++ Sources/OmniaModalView/OmniaModalView.swift
@@ -1,3 +1,163 @@
-struct OmniaModalView {
-    var text = "Hello, World!"
+
+import SwiftUI
+import Combine
+/*How to use CustomModelView
+ 
+ ZStack {
+ CustomModelView(isModalShowing: <#T##Binding<Bool>#>, backgroundColor: <#T##Binding<Color>#>,
+ modalHeight: <#T##CGFloat#>, content: <#T##() -> _#>)
+ }
+ 
+ */
+
+// MARK:- CustomModelView
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+@available(iOS 13.0, *)
+public struct OmniaModalView<Content> : View where Content : View {
+    @GestureState private var dragState = DragState.inactive
+    @Binding var isShown:Bool
+    var color: LinearGradient
+    
+    private func onDragEnded(drag: DragGesture.Value) {
+        let dragThreshold = modalHeight * (2/3)
+        if drag.predictedEndTranslation.height > dragThreshold || drag.translation.height > dragThreshold{
+            isShown = false
+        }
+    }
+    
+    var modalHeight:CGFloat = 400
+    
+    var content: () -> Content
+    
+    public init(isShown: Binding<Bool>, backgroundColor: LinearGradient, content: @escaping () -> Content) {
+        self._isShown = isShown
+        self.color = backgroundColor
+        self.content = content
+    }
+    
+    public var body: some View {
+        let drag = DragGesture()
+            .updating($dragState) { drag, state, transaction in
+                state = .dragging(translation: drag.translation)
+            }
+            .onEnded(onDragEnded)
+        return Group {
+            ZStack {
+                //Background
+                Spacer()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+                    .background(isShown ? Color.black.opacity( 0.5 * fraction_progress(lowerLimit: 0, upperLimit: Double(modalHeight), current: Double(dragState.translation.height), inverted: true)) : Color.clear)
+                    .animation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
+                    .gesture(
+                        TapGesture()
+                            .onEnded { _ in
+                                UIApplication.shared.endEditing()
+                                self.isShown = false
+                            }
+                    )
+                    .offset(y: -30)
+                
+                //Foreground
+                VStack{
+                    Spacer()
+                    ZStack{
+                        color.opacity(1.0)
+                            .frame(width: UIScreen.main.bounds.size.width, height:modalHeight)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                        self.content()
+                            .padding()
+                            .padding(.bottom, 65)
+                            .frame(width: UIScreen.main.bounds.size.width, height:modalHeight)
+                            .clipped()
+                    }
+                    .offset(y: isShown ? ((self.dragState.isDragging && dragState.translation.height >= 1) ? dragState.translation.height : 0) : modalHeight)
+                    .animation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
+                    .gesture(drag)
+                    
+                    
+                }.KeyboardAwarePadding()
+            }.edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+struct KeyboardAwareModifier: ViewModifier {
+    @State private var keyboardHeight: CGFloat = 0
+    
+    private var keyboardHeightPublisher: AnyPublisher<CGFloat, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+                .map { $0.cgRectValue.height },
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in CGFloat(0) }
+        ).eraseToAnyPublisher()
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .padding(.bottom, keyboardHeight)
+            .onReceive(keyboardHeightPublisher) { self.keyboardHeight = $0 }
+    }
+}
+
+@available(iOS 13.0, *)
+extension View {
+    func KeyboardAwarePadding() -> some View {
+        ModifiedContent(content: self, modifier: KeyboardAwareModifier())
+    }
+}
+
+enum DragState {
+    case inactive
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        switch self {
+        case .inactive:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .inactive:
+            return false
+        case .dragging:
+            return true
+        }
+    }
+}
+
+
+
+func fraction_progress(lowerLimit: Double = 0, upperLimit:Double, current:Double, inverted:Bool = false) -> Double{
+    var val:Double = 0
+    if current >= upperLimit {
+        val = 1
+    } else if current <= lowerLimit {
+        val = 0
+    } else {
+        val = (current - lowerLimit)/(upperLimit - lowerLimit)
+    }
+    
+    if inverted {
+        return (1 - val)
+        
+    } else {
+        return val
+    }
+    
 }
